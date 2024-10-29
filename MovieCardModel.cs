42b7ac04d0184edfba30c6ae0e5d876703936f2c
SnehaAkkenapally <sneha.akkenapally11@gmail.com>
2024-10-29 11:24:05 -0400

    Add files via upload

---
+++ MovieCardModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment1_MVC.Models
+{
+    public class MovieCardModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PosterUrl { get; set; }
+    }
+}
